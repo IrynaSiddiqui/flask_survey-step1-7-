eb949723b27f2b5a49f79080a6ac63e36424efbd
Iryna Siddiqui <irynasiddiqui@Irynas-iMac.local>
2025-04-04 15:00:49 -0400

    Step 1 - Step 7 finished

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+
+app = Flask(__name__)
+
+app.debug = False
+
+app.config['SECRET_KEY'] = 'porsche'
+
+toolbar = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route('/', methods=['GET', 'POST'])
+def start_survey():
+    """Starting the survey"""
+    if request.method == 'POST':
+        responses.clear()
+        return redirect('/questions/0')
+    
+    return render_template('start.html', survey=satisfaction_survey)
+
+
+@app.route('/questions/<int:question_id>')
+def show_question(question_id):
+    """Check if the question_id is valid, if not redirect to the correct URL"""
+    
+    if question_id < len(responses):
+        
+        return redirect(f"/questions/{len(responses)}")
+    
+    if question_id == len(responses):
+        if question_id < len(satisfaction_survey.questions):
+            current_question = satisfaction_survey.questions[question_id]
+            return render_template('question.html', question=current_question, question_id=question_id)
+        
+        else:
+            return redirect("/thankyou")
+    flash("Invalid question access. Please answer questions in order")
+    return redirect(f"/questions/{len(responses)}")
+
+@app.route('/answer', methods=["POST"])
+def handle_answer():
+    """Handling answer submission"""
+    answer = request.form.get("answer")
+
+    responses.append(answer)
+
+    print('Current responses:', responses)
+
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/thankyou")
+    
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+
+@app.route('/thankyou')
+def thank_you():
+    """Display thank you with surevey responses"""
+    survey_responses = zip(satisfaction_survey.questions, responses)
+    return render_template("thankyou.html", survey_responses=survey_responses)
