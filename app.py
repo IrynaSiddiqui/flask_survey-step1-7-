5ecca1a580ada45f82d7329081699b1f6cc16400
Iryna Siddiqui <irynasiddiqui@Irynas-iMac.local>
2025-04-18 14:43:17 -0400

    Used the Session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -17,7 +17,7 @@ responses = []
 def start_survey():
     """Starting the survey"""
     if request.method == 'POST':
-        responses.clear()
+        session['responses'] = []
         return redirect('/questions/0')
     
     return render_template('start.html', survey=satisfaction_survey)
@@ -26,16 +26,14 @@ def start_survey():
 @app.route('/questions/<int:question_id>')
 def show_question(question_id):
     """Check if the question_id is valid, if not redirect to the correct URL"""
-    
+    responses = session.get('responses', [])
     if question_id < len(responses):
-        
         return redirect(f"/questions/{len(responses)}")
     
     if question_id == len(responses):
         if question_id < len(satisfaction_survey.questions):
             current_question = satisfaction_survey.questions[question_id]
             return render_template('question.html', question=current_question, question_id=question_id)
-        
         else:
             return redirect("/thankyou")
     flash("Invalid question access. Please answer questions in order")
@@ -46,9 +44,11 @@ def handle_answer():
     """Handling answer submission"""
     answer = request.form.get("answer")
 
+    responses = session.get('responses', [])
+
     responses.append(answer)
 
-    print('Current responses:', responses)
+    session['responses'] = responses
 
     if len(responses) == len(satisfaction_survey.questions):
         return redirect("/thankyou")
@@ -60,5 +60,5 @@ def handle_answer():
 @app.route('/thankyou')
 def thank_you():
     """Display thank you with surevey responses"""
-    survey_responses = zip(satisfaction_survey.questions, responses)
+    survey_responses = zip(satisfaction_survey.questions, session['responses'])
     return render_template("thankyou.html", survey_responses=survey_responses)
